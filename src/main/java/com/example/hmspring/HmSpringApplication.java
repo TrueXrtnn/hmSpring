8390e8ad2628feab11603dc742fc94cdcf292641
true.xrstn <mr.chareev@mail.ru>
2023-06-14 23:30:26 +0800

    1

---
+++ src/main/java/com/example/hmspring/HmSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.hmspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hmspring/HmSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hmspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
